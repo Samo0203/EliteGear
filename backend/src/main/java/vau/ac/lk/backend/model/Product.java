f1cc4f7c37c956f62aae94d33440032f5a6a2127
Yeheni Narthana Wijesinghe <yeheninw00@gmail.com>
2026-04-29 12:04:13 +0530

    update product

--- backend/src/main/java/vau/ac/lk/backend/model/Product.java
+++ backend/src/main/java/vau/ac/lk/backend/model/Product.java
@@ -5,19 +5,25 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
 
 @Document(collection = "products")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
     @Id
     private String id;
+
     private String name;
     private String category;
     private double price;
-    private double offerprice;
-    private int stock;
+    private double offerPrice;   
+    private int    stock;
     private String description;
     private String imageUrl;
+    private List<String> colors;  
+    private List<String> sizes;
+    private List<String> weights;   
 }
